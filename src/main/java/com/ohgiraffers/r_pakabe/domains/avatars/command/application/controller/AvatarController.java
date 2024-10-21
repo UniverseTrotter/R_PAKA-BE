e714cb605493c17e334bf1b833ca8a3699e54b53
GarlGaru <juon304@gmail.com>
2024-10-21 09:51:43 +0900

    아바타 수정, 관리용 api 추가

--- src/main/java/com/ohgiraffers/r_pakabe/domains/avatars/command/application/controller/AvatarController.java
+++ src/main/java/com/ohgiraffers/r_pakabe/domains/avatars/command/application/controller/AvatarController.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Tag(name = "유저 아바타", description = "유저 아바타 관련 api")
 @RestController("/avatar")
 public class AvatarController {
@@ -24,6 +26,7 @@ public class AvatarController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공적으로 반환 되었습니다."),
             @ApiResponse(responseCode = "400", description = "해당 유저에게 아바타가 없습니다"),
+            @ApiResponse(responseCode = "401", description = "해당 유저가 없습니다"),
             @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
     })
     @GetMapping("/user")
@@ -32,10 +35,10 @@ public class AvatarController {
         return ResponseEntity.ok(avatarDTO);
     }
 
-    @Operation(summary = "작성중)회원 아바타 업로드", description = "회원코드로 아바타 정보를 업로드합니다.")
+    @Operation(summary = "회원 아바타 업로드", description = "회원코드로 아바타 정보를 업로드합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공적으로 처리 되었습니다."),
-            @ApiResponse(responseCode = "400", description = "해당 유저가 없습니다"),
+            @ApiResponse(responseCode = "401", description = "해당 유저가 없습니다"),
             @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
     })
     @PostMapping("/user/upload")
@@ -45,42 +48,44 @@ public class AvatarController {
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "작성중)회원 아바타 수정 ", description = "회원코드로 아바타 정보를 가져옵니다.")
+    @Operation(summary = "회원 아바타 수정 ", description = "회원코드로 아바타 정보를 가져옵니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공적으로 반환 되었습니다."),
             @ApiResponse(responseCode = "400", description = "해당 유저에게 아바타가 없습니다"),
+            @ApiResponse(responseCode = "401", description = "해당 유저가 없습니다"),
             @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
     })
     @PutMapping("/user/update")
-    public ResponseEntity<?> updateUserAvatar(Long userCode){
-        UserAvatarDTO avatarDTO = avatarAppService.getUserAvatar(userCode);
-        return ResponseEntity.ok(avatarDTO);
+    public ResponseEntity<?> updateUserAvatar(UserAvatarDTO avatarDTO){
+        avatarAppService.updateUserAvatar(avatarDTO);
+        return ResponseEntity.ok().build();
     }
 
 
 
 
 
-    @Operation(summary = "작성중)관리용 : 유저 아바타 리스트", description = "모든 유저 아바타 정보를 가져옵니다.")
+    @Operation(summary = "관리용 : 유저 아바타 리스트", description = "모든 유저 아바타 정보를 가져옵니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공적으로 반환 되었습니다."),
+            @ApiResponse(responseCode = "400", description = "저장된 아바타가 하나도 없습니다."),
             @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
     })
     @GetMapping("/user/all")
-    public ResponseEntity<?> getUserAvatarList(Long userCode){
-        UserAvatarDTO avatarDTO = avatarAppService.getUserAvatar(userCode);
+    public ResponseEntity<?> getUserAvatarList(){
+        List<UserAvatarDTO> avatarDTO = avatarAppService.getAllList();
         return ResponseEntity.ok(avatarDTO);
     }
 
-    @Operation(summary = "작성중)관리용 : 아바타 삭제", description = "회원 아바타 정보를 삭제합니다.")
+    @Operation(summary = "관리용 : 아바타 삭제", description = "회원 아바타 정보를 삭제합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공적으로 반환 되었습니다."),
             @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
     })
     @DeleteMapping("/delete")
     public ResponseEntity<?> deleteAvatar(Long userCode){
-        UserAvatarDTO avatarDTO = avatarAppService.getUserAvatar(userCode);
-        return ResponseEntity.ok(avatarDTO);
+        avatarAppService.deleteAvatar(userCode);
+        return ResponseEntity.ok().build();
     }
 
 
--- src/main/java/com/ohgiraffers/r_pakabe/domains/avatars/command/application/service/AvatarAppService.java
+++ src/main/java/com/ohgiraffers/r_pakabe/domains/avatars/command/application/service/AvatarAppService.java
@@ -3,12 +3,13 @@ package com.ohgiraffers.r_pakabe.domains.avatars.command.application.service;
 import com.ohgiraffers.r_pakabe.common.error.ApplicationException;
 import com.ohgiraffers.r_pakabe.common.error.ErrorCode;
 import com.ohgiraffers.r_pakabe.domains.avatars.command.application.dto.UserAvatarDTO;
-import com.ohgiraffers.r_pakabe.domains.avatars.command.application.dto.UserRequestDTO;
-import com.ohgiraffers.r_pakabe.domains.avatars.command.application.dto.UserResponseDTO;
 import com.ohgiraffers.r_pakabe.domains.avatars.command.domain.model.Avatar;
 import com.ohgiraffers.r_pakabe.domains.avatars.command.domain.service.AvatarDomainService;
 import com.ohgiraffers.r_pakabe.domains.user.command.domain.service.UserDomainService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AvatarAppService {
@@ -32,6 +33,46 @@ public class AvatarAppService {
 
     public void makeUserAvatar(UserAvatarDTO avatarDTO) {
         userDomainService.checkUserExistsByCode(avatarDTO.userCode());
-        avatarDomainService.updateAvatar(new Avatar(avatarDTO));
+        avatarDomainService.saveAvatar(new Avatar(avatarDTO));
+    }
+
+    public void updateUserAvatar(UserAvatarDTO avatarDTO) {
+        userDomainService.checkUserExistsByCode(avatarDTO.userCode());
+        Avatar existAvatar = avatarDomainService.getAvatarByUserCode(avatarDTO.userCode());
+        if (existAvatar == null) {
+            throw new ApplicationException(ErrorCode.NO_SUCH_AVATAR);
+        }
+        Avatar newAvatar = new Avatar(
+                existAvatar.getId(),
+                avatarDTO.userCode(),
+                avatarDTO.userAvatarGender(),
+                avatarDTO.userAvatarHair(),
+                avatarDTO.userAvatarBody(),
+                avatarDTO.userAvatarSkin(),
+                avatarDTO.userAvatarHand()
+        );
+
+        avatarDomainService.updateAvatar(newAvatar);
+    }
+
+    public List<UserAvatarDTO> getAllList() {
+        List<Avatar> avatarList = avatarDomainService.getAllAvatars();
+        if (avatarList == null || avatarList.isEmpty()) {
+            throw new ApplicationException(ErrorCode.NO_SUCH_AVATAR);
+        }
+        List<UserAvatarDTO> userAvatarDTOList = new ArrayList<>();
+        for (Avatar avatar : avatarList) {
+            userAvatarDTOList.add(new UserAvatarDTO(avatar));
+        }
+        return userAvatarDTOList;
+    }
+
+    public void deleteAvatar(Long userCode) {
+        userDomainService.checkUserExistsByCode(userCode);
+        Avatar avatar = avatarDomainService.getAvatarByUserCode(userCode);
+        if (avatar == null) {
+            throw new ApplicationException(ErrorCode.NO_SUCH_AVATAR);
+        }
+        avatarDomainService.deleteAvatar(avatar);
     }
 }
